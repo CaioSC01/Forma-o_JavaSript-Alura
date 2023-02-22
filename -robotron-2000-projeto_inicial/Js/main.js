4746665319be76576f377bf3db182ec1186c157f
CaioSC01 <caio180g@gmail.com>
2023-02-22 11:00:08 -0300

    começando javascript

---
+++ -robotron-2000-projeto_inicial/Js/main.js
@@ -0,0 +1,11 @@
+const robotron = document.querySelector("#robotron");
+
+robotron.addEventListener("click", () => {
+  console.log("click");
+});
+
+function dizOi(nome) {
+  console.log(nome);
+}
+
+dizOi("caio");
