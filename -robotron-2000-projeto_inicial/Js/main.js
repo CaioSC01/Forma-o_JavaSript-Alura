db43a2fd18155d48d7bb8e2e7bd40c841c3f7302
CaioSC01 <caio180g@gmail.com>
2023-02-22 13:16:48 -0300

    manipulando dados

--- -robotron-2000-projeto_inicial/Js/main.js
+++ -robotron-2000-projeto_inicial/Js/main.js
@@ -1,11 +1,15 @@
-const robotron = document.querySelector("#robotron");
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
 
-robotron.addEventListener("click", () => {
-  console.log("click");
-});
+somar.addEventListener("click", () => manipulaDados("somar"));
 
-function dizOi(nome) {
-  console.log(nome);
+subtrair.addEventListener("click", () => manipulaDados("subtrair"));
+
+function manipulaDados(operacao) {
+  if (operacao === "subtrair") {
+    braco.value = parseInt(braco.value) - 1;
+  } else {
+    braco.value = parseInt(braco.value) + 1;
+  }
 }
-
-dizOi("caio");
