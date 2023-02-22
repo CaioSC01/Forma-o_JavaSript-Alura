f47b23ff5fbe5b20baa1423043093db7b88ddfa6
CaioSC01 <caio180g@gmail.com>
2023-02-22 13:22:03 -0300

    usando forech

--- -robotron-2000-projeto_inicial/Js/main.js
+++ -robotron-2000-projeto_inicial/Js/main.js
@@ -2,12 +2,16 @@ const subtrair = document.querySelector("#subtrair");
 const somar = document.querySelector("#somar");
 const braco = document.querySelector("#braco");
 
-somar.addEventListener("click", () => manipulaDados("somar"));
+const controle = document.querySelector(".controle-ajuste");
 
-subtrair.addEventListener("click", () => manipulaDados("subtrair"));
+controle.forEach((elemento) => {
+  elemento.addEventListener("click", () => {
+    console.log(evento.target);
+  });
+});
 
 function manipulaDados(operacao) {
-  if (operacao === "subtrair") {
+  if (operacao === "-") {
     braco.value = parseInt(braco.value) - 1;
   } else {
     braco.value = parseInt(braco.value) + 1;
