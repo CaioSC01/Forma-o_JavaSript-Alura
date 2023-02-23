3022bb6741598b5ada2631257a732572abb07a96
CaioSC01 <caio180g@gmail.com>
2023-02-23 16:36:33 -0300

    usando localstorege

--- mochila-de-viagem/Js/main.js
+++ mochila-de-viagem/Js/main.js
@@ -1,11 +1,17 @@
 const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista");
+const itens = [];
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  criaElemento(
-    e.target.elements["nome"].value,
-    e.target.elements["quantidade"].value
-  );
+
+  const nome = e.target.elements["nome"];
+  const quantidade = e.target.elements["quantidade"];
+
+  criaElemento(nome.value, quantidade.value);
+
+  nome.value = "";
+  quantidade.value = "";
 });
 
 function criaElemento(nome, quantidade) {
@@ -18,7 +24,14 @@ function criaElemento(nome, quantidade) {
   novoItem.appendChild(numeroItem);
   novoItem.innerHTML += nome;
 
-  const lista = document.getElementById("lista");
-
   lista.appendChild(novoItem);
+
+  const itemAtual = {
+    nome: nome,
+    quantidade: quantidade,
+  };
+
+  itens.push(itemAtual);
+
+  localStorage.setItem("item", JSON.stringify(itens));
 }
