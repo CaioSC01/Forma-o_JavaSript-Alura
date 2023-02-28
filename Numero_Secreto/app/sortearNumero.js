61519e49a7fc744f462ee15c9a4023fdc1324809
CaioSC01 <caio180g@gmail.com>
2023-02-28 16:43:36 -0300

    arrumando aplicaçãp

--- Numero_Secreto/app/sortearNumero.js
+++ Numero_Secreto/app/sortearNumero.js
@@ -1,16 +1,15 @@
 const menorValor = 1;
-const maiorvalor = 1000;
-
+const maiorValor = 1000;
 const numeroSecreto = gerarNumeroAleatorio();
 
 function gerarNumeroAleatorio() {
-  return parseInt(Math.random() * 100);
+  return parseInt(Math.random() * maiorValor + 1);
 }
 
-console.log("numero secreto:", numeroSecreto);
+console.log("Número Secreto:", numeroSecreto);
 
-const elementoMenorValor = document.getElementById("menor_valor");
+const elementoMenorValor = document.getElementById("menor-valor");
 elementoMenorValor.innerHTML = menorValor;
 
-const elementoMaiorValor = document.getElementById("maior_valor");
-elementoMaiorValor.innerHTML = maiorvalor;
+const elementoMaiorValor = document.getElementById("maior-valor");
+elementoMaiorValor.innerHTML = maiorValor;
