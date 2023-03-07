416f8b25c51dbc46a31379b0d40c55ceb9238051
CaioSC01 <caio180g@gmail.com>
2023-03-07 16:51:30 -0300

    Aprendendo Then,Catch e FInally

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -1,5 +1,11 @@
-console.log("Hello World!!")
+var consultaCEP = fetch("https://viacep.com.br/ws/01001000/json/")
+  .then((resposta) => resposta.json())
+  .then((r) => {
+    if (r.erro) {
+      throw Error("esse cep não existe!!");
+    } else console.log(r);
+  })
+  .catch((erro) => console.log(erro))
+  .finally((mensagem) => console.log("Processamento concluido!"));
 
-function mandaMensagem(){
-	
-}
+console.log(consultaCEP);
